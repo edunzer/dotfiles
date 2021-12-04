382c3e01b1cbfbda5869eb33ab0291e2e6feb866
Ethan Dunzer <ethandunzer@yahoo.com>
2021-12-04 15:47:04 -0800

    Update desktop.sh
    echo command add

--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "*------------------------------------- Desktop Start -------------------------------------*"
+
 # Set up the desktop environment
 cp ../img/ArcPopOSLogo.jpg ~/ArcPopOSLogo.jpg
 cp ../img/MountainRangePopLogo.jpg ~/MountainRangePopLogo.jpg
