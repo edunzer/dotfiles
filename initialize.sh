afa4c56544d66002b5b8e6f4b00b7b476f69daff
Ethan Dunzer <ethandunzer@yahoo.com>
2021-12-05 11:51:47 -0800

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# progress bar function
+
+prog() {
+    local w=80 p=$1;  shift
+    # create a string of spaces, then change them to dots
+    printf -v dots "%*s" "$(( $p*$w/100 ))" ""; dots=${dots// /.};
+    # print those dots on a fixed-width space plus the percentage etc. 
+    printf "\r\e[K|%-*s| %3d %% %s" "$w" "$dots" "$p" "$*"; 
+}
+
 # Enter directory
 cd ~
 cd dotfiles/scripts/
@@ -9,6 +19,14 @@ chmod +x aptinstall.sh
 chmod +x desktop.sh
 chmod +x setup.sh
 chmod +x symlink.sh
+
+figlet "Preparing Files!"
+for x in {1..100} ; do
+    prog "$x" still working...
+    sleep .05   # do some work here
+done ; echo
+sleep 2 # Waits x second.
+clear
 
 # Fun start
 figlet "Starting Initialization!" | lolcat
