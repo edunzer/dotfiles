49202a18d3cd9a5cd97ced7af7d492b6c216809b
Ethan Dunzer <ethandunzer@yahoo.com>
2021-12-05 16:39:55 -0800

    Update desktop.sh

--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -3,11 +3,11 @@
 echo "*------------------------------------- Desktop Start -------------------------------------*"
 
 # Set up the desktop environment
-cp ../img/ArcPopOSLogo.jpg ~v/Pictures/Wallpapers/ArcPopOSLogo.jpg
-cp ../img/MountainRangePopLogo.jpg ~v/Pictures/Wallpapers/MountainRangePopLogo.jpg
+cp ../img/ArcPopOSLogo.jpg ~/ArcPopOSLogo.jpg
+cp ../img/MountainRangePopLogo.jpg ~/MountainRangePopLogo.jpg
 
 cd ~
 cd dotfiles/
 dconf load /org/gnome/ < settings.dconf
 
-gsettings set org.gnome.desktop.background picture-uri file:////home/v/dotfiles/img/ubuntu-default-greyscale-wallpaper.png
+gsettings set org.gnome.desktop.background picture-uri file:////home/v/dotfiles/img/MountainRangePopLogo.jpg
